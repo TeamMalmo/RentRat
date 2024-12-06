742de96a1dc60421c3110c0e0b45c065ec6f4810
jumistrandberg <feliciastrandberg@gmail.com>
2024-12-06 16:12:27 +0100

    Adds svg for logo and adjusts width for development purpose.

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -6,6 +6,7 @@ export default {};
 <template>
 <div>
   <h1>Welcome</h1>
+    <object type="image/svg+xml" data="../../public/images/rent-a-rat-vector.svg" class="svg-object"></object>
   <RouterLink to="/renter">
     <button>I am a Renter</button>
   </RouterLink>
@@ -14,3 +15,9 @@ export default {};
   </RouterLink>
 </div>
 </template>
+
+<style scoped>
+.svg-object {
+  max-width: 100%;
+}
+</style>

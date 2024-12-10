3312aed39afd0adc8b7ff3399a5f36594bad5d10
Jumi <feliciastrandberg@gmail.com>
2024-12-10 20:41:49 +0100

    Changes path for logo image. Removes export default and includes setup in script for consistency with composition api

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,22 +1,32 @@
+<script setup>
+import { onMounted } from 'vue';
 
-<script>
-export default {};
+onMounted(() => {
+  console.log('mounted, bitch')
+})
 </script>
 
 <template>
-<div>
-  <h1>Welcome</h1>
-    <object type="image/svg+xml" data="../../public/images/rent-a-rat-vector.svg" class="svg-object"></object>
-  <RouterLink to="/renter">
-    <button>I am a Renter</button>
-  </RouterLink>
-  <RouterLink to="/rentee">
-    <button>I am a Rentee</button>
-  </RouterLink>
-</div>
+  <div>
+    <h1 class="">Rent a Rat</h1>
+    <object
+      type="image/svg+xml"
+      data="/images/rent-a-rat-vector.svg"
+      class="svg-object"
+    ></object>
+    <RouterLink to="/renter">
+      <button>I am a Renter</button>
+    </RouterLink>
+    <RouterLink to="/rentee">
+      <button>I am a Rentee</button>
+    </RouterLink>
+  </div>
 </template>
 
-<style scoped>
+<style>
+body {
+  background-color: #8ace00;
+}
 .svg-object {
   max-width: 100%;
 }

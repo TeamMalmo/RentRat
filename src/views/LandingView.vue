4d6a4ef93807ea45142732b91e27c14a2f40ea13
Jumi <feliciastrandberg@gmail.com>
2024-12-10 22:06:44 +0100

    Adds Arial font. Adds headers and nav

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,7 +1,7 @@
 <script setup>
-import { ref } from 'vue';
+import { ref } from "vue";
 
-const isLogoLoaded = ref(false); 
+const isLogoLoaded = ref(false);
 
 const onLogoLoad = () => {
   isLogoLoaded.value = true;
@@ -10,27 +10,28 @@ const onLogoLoad = () => {
 const onLogoError = () => {
   isLogoLoaded.value = false;
 };
-
-
 </script>
 
 <template>
   <div>
-    <!-- v-bind to hide if logo loads, otherwise show -->
-    <h1 v-show="!isLogoLoaded">Rent a Rat</h1>
-    <object
+    <header>
+      <h1 v-show="!isLogoLoaded">Rent a Rat</h1>
+      <!-- <object
       type="image/svg+xml"
       data="/images/rent-a-rat-vector.svg"
       class="svg-object"
       @load="onLogoLoad"
       @error="onLogoError"
-    ></object>
-    <RouterLink to="/renter">
-      <button>I am a Renter</button>
-    </RouterLink>
-    <RouterLink to="/rentee">
-      <button>I am a Rentee</button>
-    </RouterLink>
+    ></object> -->
+    </header>
+    <nav>
+      <RouterLink to="/renter">
+        <button>I am a Renter</button>
+      </RouterLink>
+      <RouterLink to="/rentee">
+        <button>I am a Rentee</button>
+      </RouterLink>
+    </nav>
   </div>
 </template>
 

8d5a6342a2c7103da8072817887f27c31d9706a8
Jumi <feliciastrandberg@gmail.com>
2024-12-12 23:50:41 +0100

    Adds simple nav to click through tabs

--- src/components/ProfileComponents/RenterProfile.vue
+++ src/components/ProfileComponents/RenterProfile.vue
@@ -1,6 +1,37 @@
 <script setup>
+import { ref } from "vue";
+import ProfileInfo from "./ProfileInfo.vue";
+import EditRats from "../EditRats/EditRats.vue";
+
+// Open on my rats, track active tab
+const activeTab = ref("Rats");
+
+// Set tabs to their components
+const tabs = {
+  Rats: EditRats,
+  Profile: ProfileInfo,
+};
 </script>
 
 <template>
-    <h1>Profile info will be here</h1>
+  <main>
+    <h1>Welcome bajs</h1>
+    <nav>
+      <div>
+        <!-- Loop through the tabs in tabs object -->
+        <!-- Key is name of tab  -->
+        <!-- Conditionally apply active class if active tab is current tab  -->
+        <button
+          v-for="(component, tab) in tabs"
+          :key="tab"
+          :class="{ active: activeTab === tab }"
+          @click="activeTab = tab"
+        >
+          <!-- Show tab name  -->
+          {{ tab }}
+        </button>
+      </div>
+    </nav>
+    <component :is="tabs[activeTab]" />
+  </main>
 </template>
--- src/views/RenterProfileView.vue
+++ src/views/RenterProfileView.vue
@@ -1,13 +1,13 @@
 <script setup>
-import DemoList from '@/components/DeleteRats/DemoList.vue';
-import EditRats from '@/components/EditRats/EditRats.vue';
-import RenterProfile from '@/components/ProfileComponents/RenterProfile.vue';
+import DemoList from "@/components/DeleteRats/DemoList.vue";
+import EditRats from "@/components/EditRats/EditRats.vue";
+import RenterProfile from "@/components/ProfileComponents/RenterProfile.vue";
 </script>
 
 <template>
   <main>
     <h1>This is a RenterProfile page</h1>
     <RenterProfile />
-    <EditRats />
+    <!-- <EditRats /> -->
   </main>
 </template>

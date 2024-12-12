0bd019d275ee785dd2f77bd95a03d402a56a4f84
Jumi <feliciastrandberg@gmail.com>
2024-12-12 23:31:13 +0100

    Creates component for profile into and imports in profile view

---
+++ src/components/ProfileComponents/RenterProfile.vue
@@ -0,0 +1,6 @@
+<script setup>
+</script>
+
+<template>
+    <h1>Profile info will be here</h1>
+</template>
--- src/views/RenterProfileView.vue
+++ src/views/RenterProfileView.vue
@@ -1,11 +1,13 @@
 <script setup>
 import DemoList from '@/components/DeleteRats/DemoList.vue';
 import EditRats from '@/components/EditRats/EditRats.vue';
+import RenterProfile from '@/components/ProfileComponents/RenterProfile.vue';
 </script>
 
 <template>
   <main>
     <h1>This is a RenterProfile page</h1>
+    <RenterProfile />
     <EditRats />
   </main>
 </template>

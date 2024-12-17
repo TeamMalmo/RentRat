7cde49bad1f37fbeba718e876d2b318d651810ac
Jumi <feliciastrandberg@gmail.com>
2024-12-17 19:53:44 +0100

    Adds comments

--- src/views/RenteeProfileView.vue
+++ src/views/RenteeProfileView.vue
@@ -1,9 +1,11 @@
+<!-- This is the profile page for users who are renting rats and their services  -->
 <script setup>
 import EditUser from '@/components/EditUser/EditUser.vue';
 </script>
 
 <template>
   <main>
+    <!-- The component to edit user information  -->
     <EditUser />
   </main>
 </template>

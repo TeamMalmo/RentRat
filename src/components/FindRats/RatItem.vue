cde3ee5ab4b432d8a277c1ebd27458a7f0e4007e
Jumi <feliciastrandberg@gmail.com>
2024-12-17 01:41:49 +0100

    Adds basic favorite button

--- src/components/FindRats/RatItem.vue
+++ src/components/FindRats/RatItem.vue
@@ -1,4 +1,14 @@
 <script setup>
+import { ref } from "vue";
+import GlowButton from "../GlowButton.vue";
+
+const isFavorite = ref(false);
+
+const toggleFavorite = (event) => {
+  // Stop click from opening rat details
+  event.stopPropagation();
+  isFavorite.value = !isFavorite.value;
+};
 
 defineProps({
   rat: Object, // Data för en specifik råtta
@@ -7,49 +17,58 @@ defineProps({
 
 <template>
   <li class="rat-list-item">
-      <span>
-        <h2>{{ rat.name }} in {{ rat.areaOfMalmo }}</h2>
-        <p>Primary Skill: {{ rat.primarySkill }}</p>
-        <p>Other Skills: {{ rat.skills.join(', ') }}</p>
-        <p>Available? {{ rat.availability ? 'Yes' : 'No' }}</p>
-        <p>Renter: {{ rat.renter }}</p>
-      </span>
-      <span>
-        <img :src="rat.imgUrl" alt="rat-image" class="rat-image" />
-        <p>Hourly Rate: {{ rat.price }}:-</p>
-      </span>
+    <span>
+      <h2>{{ rat.name }} in {{ rat.areaOfMalmo }}</h2>
+      <p>Primary Skill: {{ rat.primarySkill }}</p>
+      <p>Other Skills: {{ rat.skills.join(", ") }}</p>
+      <p>Available? {{ rat.availability ? "Yes" : "No" }}</p>
+      <p>Renter: {{ rat.renter }}</p>
+      <button class="favorite-btn" @click="toggleFavorite">
+        {{ isFavorite ? "Unfavorite💔" : "Favorite❤️" }}
+      </button>
+    </span>
+    <span>
+      <img :src="rat.imgUrl" alt="rat-image" class="rat-image" />
+      <p>Hourly Rate: {{ rat.price }}:-</p>
+    </span>
   </li>
 </template>
 
 <style scoped>
-
-*{
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box ;
-        text-transform:lowercase;
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+  text-transform: lowercase;
 }
 
 .rat-list-item {
-        display: flex;
-        /* flex-direction: column; */
-        align-items: center;
-        justify-content: space-between;
-        border-radius: 0.5rem;
-        padding: 0.5rem;
-        /* margin: 0.5rem; */
-        width: 500px;
-        background-color: #8ACE00;
-    }
+  border: 1px solid pink;
+  display: flex;
+  /* flex-direction: column; */
+  align-items: center;
+  justify-content: space-between;
+  border-radius: 0.5rem;
+  padding: 0.5rem;
+  /* margin: 0.5rem; */
+  width: 500px;
+  background-color: #8ace00;
+}
 
-    .rat-list-item:hover{
-        transform: scale(1.05);
-        transition: 0.2s ease-in-out;
-        cursor: pointer;
-    }
+.rat-list-item:hover {
+  transform: scale(1.05);
+  transition: 0.2s ease-in-out;
+  cursor: pointer;
+}
 
-    .rat-image {
-        width: 100px;
-        border-radius: 100%;
-    }
+.rat-image {
+  width: 100px;
+  border-radius: 100%;
+}
+
+.favorite-btn {
+  width: 9rem;
+  height: 3.5rem;
+  padding: 5%;
+}
 </style>
